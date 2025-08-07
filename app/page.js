664387c520070fd0cb91512679b734b7635cc5b6
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-07 19:27:55 -0400

    Put all the data into json file.

--- app/page.js
+++ app/page.js
@@ -7,103 +7,10 @@ import SubTitle from "@/components/SubTitle";
 import Title from "@/components/Title";
 import Wrapper from "@/components/Wrapper";
 
+import skills from "@/data/skills.json";
+import socials from "@/data/socials.json";
+
 export default function Home() {
-  const socialLinks = [
-    {
-      id: 1,
-      iconSrc: "/github-icon.png",
-      title: "GitHub",
-      link: "https://github.com/joewardperalta/",
-    },
-    {
-      id: 2,
-      iconSrc: "/linkedin-icon.png",
-      title: "LinkedIn",
-      link: "https://www.linkedin.com/in/joewardperalta/",
-    },
-    {
-      id: 3,
-      iconSrc: "/briefcase-icon.png",
-      title: "Portfolio",
-      link: "https://www.joewardperalta.dev/",
-    },
-  ];
-
-  const skills = [
-    {
-      id: 1,
-      iconSrc: "/html-icon.png",
-      title: "HTML",
-      link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-    },
-    {
-      id: 2,
-      iconSrc: "/css-icon.png",
-      title: "CSS",
-      link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-    },
-    {
-      id: 3,
-      iconSrc: "/javascript-icon.png",
-      title: "Javascript",
-      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    },
-    {
-      id: 4,
-      iconSrc: "/reactjs-icon.png",
-      title: "React.js",
-      link: "https://react.dev/",
-    },
-    {
-      id: 5,
-      iconSrc: "/nextjs-icon.png",
-      title: "Next.js",
-      link: "https://nextjs.org/",
-    },
-    {
-      id: 6,
-      iconSrc: "/tailwindcss-icon.png",
-      title: "Tailwind CSS",
-      link: "https://tailwindcss.com/",
-    },
-    {
-      id: 7,
-      iconSrc: "/nodejs-icon.png",
-      title: "Node.js",
-      link: "https://nodejs.org/en",
-    },
-    {
-      id: 8,
-      iconSrc: "/expressjs-icon.png",
-      title: "Express.js",
-      link: "https://expressjs.com/",
-    },
-    {
-      id: 9,
-      iconSrc: "/mongodb-icon.png",
-      title: "MongoDB",
-      link: "https://www.mongodb.com/",
-    },
-    {
-      id: 10,
-      iconSrc: "/git-icon.png",
-      title: "Git",
-      link: "https://git-scm.com/",
-    },
-    {
-      id: 11,
-      iconSrc: "/github-icon.png",
-      title: "GitHub",
-      link: "https://github.com/",
-    },
-    {
-      id: 12,
-      iconSrc: "/figma-icon.png",
-      title: "Figma",
-      link: "https://www.figma.com/",
-    },
-  ];
-
   return (
     <main>
       {/* Hero Section */}
@@ -123,12 +30,12 @@ export default function Home() {
 
             {/* Social links */}
             <ul className="flex gap-6">
-              {socialLinks.map((socialLink) => (
-                <li key={socialLink.id}>
+              {socials.map((social) => (
+                <li key={social.id}>
                   <SocialLinkCard
-                    iconSrc={socialLink.iconSrc}
-                    title={socialLink.title}
-                    link={socialLink.link}
+                    iconSrc={social.iconSrc}
+                    title={social.title}
+                    link={social.link}
                   />
                 </li>
               ))}
@@ -166,6 +73,12 @@ export default function Home() {
               </li>
             ))}
           </ul>
+        </Wrapper>
+      </section>
+
+      <section>
+        <Wrapper>
+          <SectionHeading>Projects</SectionHeading>
         </Wrapper>
       </section>
     </main>
