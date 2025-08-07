d3131d038da3e3f9a683e52c9c210af3b6c62f49
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-07 14:37:10 -0400

    Create the profile photo component.

---
+++ components/ProfilePhoto.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function ProfilePhoto({ src }) {
+  return (
+    <div className="w-[62px] h-[62px] md:w-[70px] md:h-[70px] rounded-lg overflow-hidden">
+      <Image
+        className="w-full h-full"
+        src={src}
+        alt="Author's profile photo."
+        width={70}
+        height={70}
+      />
+    </div>
+  );
+}
