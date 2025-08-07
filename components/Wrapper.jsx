5dd37725f5c4f3d3e8c3fedd8dac86d9b7336757
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-07 16:55:42 -0400

    Add borders to the wrapper.

--- components/Wrapper.jsx
+++ components/Wrapper.jsx
@@ -1,6 +1,8 @@
 export default function Wrapper({ className = "", children }) {
   return (
-    <div className={`${className} py-6 px-4 max-w-[800px] mx-auto md:p-8`}>
+    <div
+      className={`${className} py-6 px-4 border-[0.5px] border-tertiary max-w-[800px] mx-auto md:p-8`}
+    >
       {children}
     </div>
   );
