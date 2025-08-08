6c92ff8086e0fdde474299598698e5574eb3177e
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-07 22:57:29 -0400

    Create the education card component and add the education data to the education.json.

---
+++ components/EducationCard.jsx
@@ -0,0 +1,38 @@
+import Paragraph from "./Paragraph";
+import SubHeading from "./SubHeading";
+
+export default function EducationCard({ className, education }) {
+  const educationDateText =
+    education.startDate.month +
+    " " +
+    education.startDate.year +
+    " - " +
+    (education.endDate.month
+      ? education.endDate.month + " " + education.endDate.year
+      : "Present");
+
+  return (
+    <div className="space-y-2">
+      {/* Headline */}
+      <div>
+        {/* Title */}
+        <SubHeading>{education.title}</SubHeading>
+
+        {/* Date and stack */}
+        <div>
+          <p className="font-medium text-xs uppercase text-secondary">
+            {educationDateText}
+          </p>
+        </div>
+      </div>
+
+      {/* Responsibilities */}
+      <div>
+        <Paragraph>
+          <span className="font-medium">Coursework: </span>
+          {education.coursework}
+        </Paragraph>
+      </div>
+    </div>
+  );
+}
