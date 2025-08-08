0973090ede0472edb383d47b5cd793dcb4c37426
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-07 23:14:39 -0400

    Create the footer component and add it to the layout page.

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import Paragraph from "./Paragraph";
+
+export default function Footer() {
+  return (
+    <footer className="py-3 px-4">
+      <Paragraph>&copy; 2025 Resume by Joeward Peralta</Paragraph>
+    </footer>
+  );
+}
