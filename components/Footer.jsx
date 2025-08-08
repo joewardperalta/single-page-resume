bde3bd7b8416100e433f7e795e538acc39442023
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-07 23:34:23 -0400

    Fix the border

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,12 @@
 import Paragraph from "./Paragraph";
+import Wrapper from "./Wrapper";
 
 export default function Footer() {
   return (
-    <footer className="py-3 px-4">
-      <Paragraph>&copy; 2025 Resume by Joeward Peralta</Paragraph>
+    <footer>
+      <div className="py-3 px-4 max-w-[800px] mx-auto border-tertiary border-[0.5px]">
+        <Paragraph>&copy; 2025 Resume by Joeward Peralta</Paragraph>
+      </div>
     </footer>
   );
 }
--- components/Wrapper.jsx
+++ components/Wrapper.jsx
@@ -1,7 +1,7 @@
 export default function Wrapper({ className = "", children }) {
   return (
     <div
-      className={`${className} py-6 px-4 border-[0.5px] border-tertiary max-w-[800px] mx-auto`}
+      className={`${className} py-6 px-4 border-[0.5px] border-b-[0] border-tertiary max-w-[800px] mx-auto`}
     >
       {children}
     </div>
