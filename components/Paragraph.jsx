39505a607ef32c0c46f8f04b53d657ea3f1a3e96
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-08 10:47:59 -0400

    Increase the size of the paragraph to 16px when viewing in tablet and desktop device.

--- components/Paragraph.jsx
+++ components/Paragraph.jsx
@@ -1,3 +1,7 @@
 export default function Paragraph({ className = "", children }) {
-  return <p className={`${className} text-sm text-secondary`}>{children}</p>;
+  return (
+    <p className={`${className} text-sm text-secondary md:text-base`}>
+      {children}
+    </p>
+  );
 }
