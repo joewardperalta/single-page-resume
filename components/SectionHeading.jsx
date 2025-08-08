bc6a15db970d36997de4b161bace9191e82e2be2
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-08 10:59:36 -0400

    Increase the text size of section heading when viewing in tablet and desktop device.

--- components/SectionHeading.jsx
+++ components/SectionHeading.jsx
@@ -1,6 +1,8 @@
 export default function SectionHeading({ children, className }) {
   return (
-    <h2 className={`${className} uppercase text-xs font-medium mb-3`}>
+    <h2
+      className={`${className} uppercase text-xs font-medium mb-3 md:text-sm`}
+    >
       {children}
     </h2>
   );
