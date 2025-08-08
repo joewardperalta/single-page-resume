a2226c4985e1c3a9a9ea20d01207c0ab0cecf59a
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-07 20:24:28 -0400

    Add a github link and website link to the ProjectCard component so that users or recruiters can easily navigate to the github repo and the live website.

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import Paragraph from "./Paragraph";
+import SocialLinkCard from "./SocialLinkCard";
 import SubHeading from "./SubHeading";
 
 export default function ProjectCard({ className, project }) {
@@ -6,20 +7,41 @@ export default function ProjectCard({ className, project }) {
     <div>
       {/* Headline */}
       <div className="mb-1">
-        <SubHeading>{project.title}</SubHeading>
-        <p className="font-medium text-xs uppercase text-secondary">
-          {project.startDate.month} {project.startDate.year} {" - "}
-          {project.endDate.month
-            ? project.endDate.month + " " + project.endDate.year
-            : "Present"}
-          {" | "}
-          {project.stacks.map((stack, index) =>
-            index < project.stacks.length ? stack + "," : stack
-          )}
-        </p>
+        {/* Title */}
+        <div className="flex justify-between">
+          <SubHeading>{project.title}</SubHeading>
+
+          {/* Links */}
+          <div className="flex gap-4">
+            <SocialLinkCard
+              iconSrc="/github-icon.png"
+              title="GitHub"
+              link={project.githubLink}
+            />
+            <SocialLinkCard
+              iconSrc="/link-icon.png"
+              title="Website"
+              link={project.websiteLink}
+            />
+          </div>
+        </div>
+
+        {/* Date and stack */}
+        <div>
+          <p className="font-medium text-xs uppercase text-secondary">
+            {project.startDate.month} {project.startDate.year} {" - "}
+            {project.endDate.month
+              ? project.endDate.month + " " + project.endDate.year
+              : "Present"}
+            {" | "}
+            {project.stacks.map((stack, index) =>
+              index < project.stacks.length ? stack + "," : stack
+            )}
+          </p>
+        </div>
       </div>
 
-      {/* Description */}
+      {/* Responsibilities */}
       <div className="pl-6">
         <ul>
           {project.responsibilities.map((responsibility, index) => (
