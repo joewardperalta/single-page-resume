30dc98c140119bbd7a39eb0ce60c540f044d4522
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-08 16:27:54 -0400

    Add a scroll animation

--- components/Wrapper.jsx
+++ components/Wrapper.jsx
@@ -1,9 +1,22 @@
+"use client";
+import { motion } from "framer-motion";
+
 export default function Wrapper({ className = "", children }) {
   return (
-    <div
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{ once: true }}
       className={`${className} py-6 px-4 border-[0.5px] border-b-[0] border-tertiary max-w-[800px] mx-auto md:p-8`}
     >
       {children}
-    </div>
+    </motion.div>
   );
 }
