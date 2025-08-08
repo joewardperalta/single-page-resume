58cf5ec9f5abfa0b4065ecae573f1ca214a7c8d2
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-08 11:22:34 -0400

    Change the text transform for the MiniHeading from uppercase to capitalize.

--- components/SubHeading.jsx
+++ components/SubHeading.jsx
@@ -1,6 +1,6 @@
 export default function SubHeading({ children, className }) {
   return (
-    <h3 className={`${className} font-medium text-sm uppercase md:text-base`}>
+    <h3 className={`${className} font-medium text-sm capitalize md:text-base`}>
       {children}
     </h3>
   );
