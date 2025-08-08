055376a2fad1ca5341063b35f766aeec3d9e36c8
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-08 10:45:06 -0400

    Change the pixels to rem

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,6 +1,6 @@
 export default function Title({ children, className }) {
   return (
-    <h1 className={`${className} text-2xl md:text-[26px] font-semibold`}>
+    <h1 className={`${className} text-2xl md:text-[1.625rem] font-semibold`}>
       {children}
     </h1>
   );
