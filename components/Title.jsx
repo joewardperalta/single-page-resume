893d0a86e5a9ba88c0b99744906e5ae72ed198da
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-08 10:44:21 -0400

    Increase the size of title to 26px when viewing in tablet and desktop size.

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,3 +1,7 @@
 export default function Title({ children, className }) {
-  return <h1 className={`${className} text-2xl font-semibold`}>{children}</h1>;
+  return (
+    <h1 className={`${className} text-2xl md:text-[26px] font-semibold`}>
+      {children}
+    </h1>
+  );
 }
